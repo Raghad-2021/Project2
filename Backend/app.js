dae8318bc534d596c176235a79f72e3c9f8846ef
IbtisamAlruwili <Ibtisam.Ghazi.rae.js@tuwaiq.edu.sa>
2021-11-17 09:24:31 +0300

    like

--- Backend/app.js
+++ Backend/app.js
@@ -6,14 +6,14 @@ app.use(express.json());
 
 app.use(cors());
 
-const Dispensaries = [{id:1,name:"مجمع عيادات سلامتي",img:"https://2u.pw/IAosQ",Location:"عرعر- حي المحمدية ",location1:"https://goo.gl/maps/oeUj6UvJqkH2uCJ77" ,service:["عيادة جلدية ","عيادة الليز ","عيادة التجميل"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/u3qyg", "https://2u.pw/IDD3G","https://2u.pw/AgleZ"]},
-                    {id:2,name:"مجمع اماس الطبي",img:"https://2u.pw/zLGr0",Location:"عرعر- حي العزيزيه", location1:"https://g.page/amassmedicalcomplex?share" ,service:["عيادة جلدية ","عيادة أسنان ","عيادة العظام","عيادة المسالك","عيادة الباطنية"," عيادة النساء والولادة "],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/DJUAW", "https://2u.pw/h5Cbr","https://2u.pw/jfRpN"]},
-                    {id:3,name:"مجمع سحاب الشمال الطبي",img:"https://2u.pw/wRLs4",Location:"عرعر-حي الناصرية",location1:"https://goo.gl/maps/5PCygVP5PXJgcvLg9" ,service:[" عيادة طؤارى" ," مختبر","عيادة أشعة","عيادة أطفال","عيادة جلدية","عيادة باطنية","عيادة أسنان","عيادة طب عام","عيادة عيون ","عيادة جراحة"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/sRSIc", "https://2u.pw/7drvN","https://2u.pw/U49Hp"]},
-                    {id:4,name:"مستوصف الخناني الطبي",img:"https://2u.pw/f3zpG",Location:"عرعر- حي العزيزية",location1:"https://goo.gl/maps/Vykj8yNJn9VpHRoH9",service:[" عيادة طب العيون " , " عيادة طب الاسنان ", "عيادة طب العظام", " المختبر" ,"عيادة الباطنية ", "عيادة الاشعة " ,"عيادة الامراض الصدرية ","عيادة الانف والاذن والحنجرة"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/g0Eec", "",""]},
-                    {id:5,name:"مجمع لين الخاص الطبي",img:"https://2u.pw/JrXzF",Location:"عرعر- حي المحمدية ",location1:"https://goo.gl/maps/NuMEgE9WSgrnQnKU6",service:[ " عيادة طب الاسنان ", "عيادة طب العظام", " عيادة النساء و الولادة" ,"عيادة الباطنية ", "عيادة الاشعة " ,"عيادة الامراض الصدرية ","عيادة الانف والاذن والحنجرة"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/rWFKC", "https://2u.pw/Q9ltL","https://2u.pw/69exi"]},
-                    {id:7,name:"مجمع اليحيي الطبي",img:"https://2u.pw/jLpHZ",Location:"عرعر-حي المساعدية",location1:"https://goo.gl/maps/NC4SeL82qe6rTVnD8",service:["عيادة الاسنان ","عيادة طب عام  ","عيادة الاطفال"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/0gBqS", "https://2u.pw/PkFrd","https://2u.pw/3bcKQ"]},
-                    {id:8,name:"المهيدب لطب الاسنان ",img:"https://2u.pw/ga7cp",Location:"عرعر-حي المساعدية",location1:"https://goo.gl/maps/22sf21wy6Nt49QhM7",service:["عيادة الأسنان  ","عيادة جلدية ","عيادة التجميل"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/KTYbs", "https://2u.pw/hk85P","https://2u.pw/IbIxL"]},
-                    {id:10,name:"مجمع الخبرة الطبي ",img:"https://2u.pw/a4G0q",Location:"عرعر-حي المساعدية",location1:"https://goo.gl/maps/KTbpWLkMjrNCMaMf8",service:["عيادة جلدية ","عيادة الليز ","عيادة التجميل"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/VGFaY", "",""]},
+const Dispensaries = [{id:1,key:false,name:"مجمع عيادات سلامتي",img:"https://2u.pw/IAosQ",Location:"عرعر- حي المحمدية ",location1:"https://goo.gl/maps/oeUj6UvJqkH2uCJ77" ,service:["عيادة جلدية ","عيادة الليز ","عيادة التجميل"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/u3qyg", "https://2u.pw/IDD3G","https://2u.pw/AgleZ"]},
+                    {id:2,key:false,name:"مجمع اماس الطبي",img:"https://2u.pw/zLGr0",Location:"عرعر- حي العزيزيه", location1:"https://g.page/amassmedicalcomplex?share" ,service:["عيادة جلدية ","عيادة أسنان ","عيادة العظام","عيادة المسالك","عيادة الباطنية"," عيادة النساء والولادة "],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/DJUAW", "https://2u.pw/h5Cbr","https://2u.pw/jfRpN"]},
+                    {id:3,key:false,name:"مجمع سحاب الشمال الطبي",img:"https://2u.pw/wRLs4",Location:"عرعر-حي الناصرية",location1:"https://goo.gl/maps/5PCygVP5PXJgcvLg9" ,service:[" عيادة طؤارى" ," مختبر","عيادة أشعة","عيادة أطفال","عيادة جلدية","عيادة باطنية","عيادة أسنان","عيادة طب عام","عيادة عيون ","عيادة جراحة"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/sRSIc", "https://2u.pw/7drvN","https://2u.pw/U49Hp"]},
+                    {id:4,key:false,name:"مستوصف الخناني الطبي",img:"https://2u.pw/f3zpG",Location:"عرعر- حي العزيزية",location1:"https://goo.gl/maps/Vykj8yNJn9VpHRoH9",service:[" عيادة طب العيون " , " عيادة طب الاسنان ", "عيادة طب العظام", " المختبر" ,"عيادة الباطنية ", "عيادة الاشعة " ,"عيادة الامراض الصدرية ","عيادة الانف والاذن والحنجرة"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/g0Eec", "",""]},
+                    {id:5,key:false,name:"مجمع لين الخاص الطبي",img:"https://2u.pw/JrXzF",Location:"عرعر- حي المحمدية ",location1:"https://goo.gl/maps/NuMEgE9WSgrnQnKU6",service:[ " عيادة طب الاسنان ", "عيادة طب العظام", " عيادة النساء و الولادة" ,"عيادة الباطنية ", "عيادة الاشعة " ,"عيادة الامراض الصدرية ","عيادة الانف والاذن والحنجرة"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/rWFKC", "https://2u.pw/Q9ltL","https://2u.pw/69exi"]},
+                    {id:7,key:false,name:"مجمع اليحيي الطبي",img:"https://2u.pw/jLpHZ",Location:"عرعر-حي المساعدية",location1:"https://goo.gl/maps/NC4SeL82qe6rTVnD8",service:["عيادة الاسنان ","عيادة طب عام  ","عيادة الاطفال"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/0gBqS", "https://2u.pw/PkFrd","https://2u.pw/3bcKQ"]},
+                    {id:8,key:false,name:"المهيدب لطب الاسنان ",img:"https://2u.pw/ga7cp",Location:"عرعر-حي المساعدية",location1:"https://goo.gl/maps/22sf21wy6Nt49QhM7",service:["عيادة الأسنان  ","عيادة جلدية ","عيادة التجميل"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/KTYbs", "https://2u.pw/hk85P","https://2u.pw/IbIxL"]},
+                    {id:10,key:false,name:"مجمع الخبرة الطبي ",img:"https://2u.pw/a4G0q",Location:"عرعر-حي المساعدية",location1:"https://goo.gl/maps/KTbpWLkMjrNCMaMf8",service:["عيادة جلدية ","عيادة الليز ","عيادة التجميل"],times:["9:00am - 12:00pm", "1:00pm - 9:00pm "],phto:["https://2u.pw/VGFaY", "",""]},
                   ]
 
 app.get("/Dispensaries", (req, res) => {
@@ -23,15 +23,34 @@ app.get("/Dispensaries", (req, res) => {
 app.get("/dates/:id",(req,res)=>{
   let resArr = []
   const idd = req.params.id
-  console.log(idd)
 for(let i=0;i<Dispensaries.length;i++){
   if(Dispensaries[i].id == idd){
     resArr.push(Dispensaries[i])
   }}
   res.status(200)
-  console.log(resArr)
 res.json(resArr)
 })
+
+app.post("/like/:id",(req,res)=>{
+  const id = req.params.id
+  console.log(id)
+for(let i=0;i<Dispensaries.length;i++){
+  if(Dispensaries[i].id == id){
+    if(Dispensaries[i].key==true){
+      Dispensaries[i].key=false}
+      else{
+        Dispensaries[i].key=true
+      
+    }
+  }}
+  res.status(200)
+res.json(Dispensaries)
+})
+
+
+
+
+
 
 const port = 5000;
 app.listen(port, () => {
--- app5/src/Components/Dispensaries/Dispensaries.jsx
+++ app5/src/Components/Dispensaries/Dispensaries.jsx
@@ -1,22 +1,27 @@
 import React, {useState ,useEffect}from 'react'
 import axios from 'axios';
 import Navbar from '../Navbar/Navbar';
+import { useParams } from "react-router-dom";
 // import {AiTwotoneHeart} from "react-icons/ai"
 import { useHistory } from "react-router-dom";
+import { logDOM } from '@testing-library/dom';
 
 // import {AiTwotoneHeart} from "react-icons/ai"
-import { useHistory } from "react-router-dom";
 export default function Dispensaries() {
   const [DispensariesArr, setDispensariesArr] = useState([])
   const history = useHistory();
   const [valueInput, setvalueInput] = useState('')
+  const p = useParams();
 
+      console.log(p,"pppppp");
+
+ 
   useEffect(async() => {
     const Dispensaries = await axios.get("http://localhost:5000/Dispensaries")
    setDispensariesArr(Dispensaries.data)
       }, [])
       const goToCard  =(id)=>{
-        history.push(`/card/${id}`);
+        // history.push(`/card/${id}`);
       }
       function setvalue(e) {
         let v = e.target.value;
@@ -26,7 +31,32 @@ export default function Dispensaries() {
           let newArr = DispensariesArr.filter((item)=>item.name ==valueInput );
           setDispensariesArr(newArr)
   }
+  const likefun=async(id)=>{
+  
+      console.log(id);
+  const likes = await axios.post(`http://localhost:5000/like/${id}`)
+  setDispensariesArr(likes.data)
+  console.log(likes,"llllllllllllllllllllllllllllllll")
+    
+  }
+// const replaceImg = (elem, index) => {
+// for (let i = 0; i < cardsArray.length; i++) {
+//     if (cardsArray[i].id == id) {
+//         if (cardsArray[i].toggle == true) {
+//             newArr[i].toggle = false
+//         } else {
+//             newArr[i].toggle = true
+//         }
+//     }
+// }
+// setCardsArray(newArr)
+
+
+
+
+// }}
     return (
+      
       <>
       <Navbar myFunction={serch} fun={ setvalue}/>
       
@@ -41,6 +71,9 @@ export default function Dispensaries() {
             <p className="">{elem.name}
             <div style={{height:"200px"}}>
            <img className="w-100 h-100" src={elem.img} alt="mj" /></div></p>
+         {  DispensariesArr[i].key === true ?  <button  onClick={()=>{likefun(elem.id)}}> 🧡</button> :  <button  onClick={()=>{likefun(elem.id)}}> 💔</button>}
+         {/* <button  onClick={()=>{likefun(elem.id)}}> 🧡</button> */}
+ 
            {/* <p>{elem.Location}</p> */}
            {/* <AiTwotoneHeart  style={{color:'red' }} /> <br/> */}
             {/* <a href={elem.location1}>Google MAP </a> */}
