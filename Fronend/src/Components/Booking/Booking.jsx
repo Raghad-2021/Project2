2e109c424bf485bac27bacc7491ec5daca9e274b
IbtisamAlruwili <Ibtisam.Ghazi.rae.js@tuwaiq.edu.sa>
2021-11-18 15:15:53 +0300

    final Project

--- Fronend/src/Components/Booking/Booking.jsx
+++ Fronend/src/Components/Booking/Booking.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Booking() {
     return (
         <div>
-            <h2>Dates</h2>
+          
         </div>
     )
 }
--- Fronend/src/Components/Footer/Footer.jsx
+++ Fronend/src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div style={{color :"white" , height:"150px "}} className="bg-dark p-3  d-flex justify-content-center align-items-center ">
-            <h3> Footer</h3>
+            <h3> this is a Footer</h3>
         </div>
     )
 }
